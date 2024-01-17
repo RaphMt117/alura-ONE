beb2bb1dccd67903f3079d6e7f67aeca925a5857
RaphMt <rafa.mtorres117@outlook.com>
2024-01-17 18:03:46 -0300

    iniciei funcoes e listas

---
+++ funcoes-listas-js/app.js
@@ -0,0 +1,15 @@
+// definição de título
+let titulo = document.getElementById('titulo')
+titulo.innerHTML = 'Jogo do número secreto'
+
+let paragrafo = document.querySelector('p')
+paragrafo.innerHTML = 'Escolha um número de 1 a 10'
+
+function verificarChute() {
+    alert('o botao foi clicado')
+    if (chute == numeroSecreto){
+        
+    }
+}
+
+let reiniciar = document.getElementById('reiniciar')
